609e1ebf807eceb0d01cd16f076d94697d76949e
AndreasLC <andreaslykkecaspers@gmail.com>
2026-04-19 17:16:08 +0200

    PlayerPlugin.java

--- Player/src/main/java/dk/sdu/cbse/playersystem/PlayerPlugin.java
+++ Player/src/main/java/dk/sdu/cbse/playersystem/PlayerPlugin.java
@@ -1,4 +1,32 @@
 package dk.sdu.cbse.playersystem;
 
-public class PlayerPlugin {
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.services.IGamePluginService;
+
+public class PlayerPlugin implements IGamePluginService {
+    private Entity player;
+    @Override
+    public void start(GameData gameData, World world) {
+        // Create entity player
+        player = createPlayerShip(gameData);
+        world.addEntity(player);
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        world.removeEntity(player); // removes player entity
+
+    }
+
+    private Entity createPlayerShip(GameData gameData)
+    {
+        Entity player = new Entity();
+        player.setPolygonCoordinates(-5,-5,10,0,-5,5); // makes a triangle spaceship.
+        player.setX(gameData.getDisplayHeight()/2); // to make sure that the player spawn in the middle of the screen.
+        player.setY(gameData.getDisplayHeight()/2);
+        player.setRadius(8f);
+        return player;
+    }
 }
