de647f6711e3f11bdb9250c173b15f9e55dba651
AndreasLC <andreaslykkecaspers@gmail.com>
2026-04-19 23:34:24 +0200

    Game now has asteroids

---
+++ Asteroids/src/main/java/dk/sdu/cbse/asteroid/AsteroidProcessor.java
@@ -0,0 +1,45 @@
+package dk.sdu.cbse.asteroid;
+
+import dk.sdu.cbse.asteroid.AsteroidSplitterImpl;
+import dk.sdu.cbse.common.asteroids.Asteroid;
+import dk.sdu.cbse.common.asteroids.IAsteroidSplitter;
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.services.IEntityProcessingService;
+
+public class AsteroidProcessor implements IEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+
+        for (Entity asteroid : world.getEntities(Asteroid.class)) {
+            double changeX = Math.cos(Math.toRadians(asteroid.getRotation()));
+            double changeY = Math.sin(Math.toRadians(asteroid.getRotation()));
+
+            asteroid.setX(asteroid.getX() + changeX * 0.5);
+            asteroid.setY(asteroid.getY() + changeY * 0.5);
+
+            // Bug fix: + i stedet for - så asteroiden wrapper til modsat kant
+            if (asteroid.getX() < 0) {
+                asteroid.setX(asteroid.getX() + gameData.getDisplayWidth());
+            }
+
+            if (asteroid.getX() > gameData.getDisplayWidth()) {
+                asteroid.setX(asteroid.getX() % gameData.getDisplayWidth());
+            }
+
+            // Bug fix: + i stedet for - så asteroiden wrapper til modsat kant
+            if (asteroid.getY() < 0) {
+                asteroid.setY(asteroid.getY() + gameData.getDisplayHeight());
+            }
+
+            if (asteroid.getY() > gameData.getDisplayHeight()) {
+                asteroid.setY(asteroid.getY() % gameData.getDisplayHeight());
+            }
+
+        }
+    }
+    // Undersøg OSGi
+}
+
+
