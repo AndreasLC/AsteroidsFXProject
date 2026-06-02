ef89bd0ef925872323ca1f66717e83c5d6d189ab
AndreasLC <andreaslykkecaspers@gmail.com>
2026-06-02 11:14:20 +0200

    ;)

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -2,8 +2,8 @@ module Core {
     requires Common;
     requires javafx.graphics;
     requires spring.context;
-    requires  spring.core;
-    requires  spring.beans;
+    requires spring.core;
+    requires spring.beans;
     opens dk.sdu.cbse to javafx.graphics, spring.core, spring.beans, spring.context;
     uses dk.sdu.cbse.common.services.IGamePluginService;
     uses dk.sdu.cbse.common.services.IEntityProcessingService;
