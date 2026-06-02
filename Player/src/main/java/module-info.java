9a300cc680098c7d3e42ab08a94bc783667ba9b7
AndreasLC <andreaslykkecaspers@gmail.com>
2026-06-02 09:43:50 +0200

    Removed wrong export in module-info found when writing report.

--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -4,7 +4,6 @@ module Player {
     requires Common;
     requires CommonBullet;
     uses dk.sdu.cbse.common.bullet.BulletSPI;
-    exports dk.sdu.cbse.playersystem;
     provides IGamePluginService with dk.sdu.cbse.playersystem.PlayerPlugin;
     provides IEntityProcessingService with dk.sdu.cbse.playersystem.PlayerControlSystem;
 }
